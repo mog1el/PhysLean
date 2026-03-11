1e601835aa56987dd29f3f39dab4ce7a5ddacff1
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2026-03-11 11:47:11 +0000

    refactor: Golf

--- PhysLean/SpaceAndTime/Space/Module.lean
+++ PhysLean/SpaceAndTime/Space/Module.lean
@@ -683,8 +683,7 @@ lemma basis_eq_mfderiv_modelDiffeo_single (d : ℕ) (μ : Fin d) (x : Space d) :
         (EuclideanSpace.single μ 1) := by
   simp [mfderiv]
   rw [if_pos (modelDiffeo.mdifferentiable (WithTop.top_ne_zero)).mdifferentiableAt]
-  change _ = fderiv ℝ (↑(manifoldStructure d).symm) ((manifoldStructure d) x)
-    (EuclideanSpace.single μ 1)
+  change _ = fderiv ℝ (manifoldStructure d).symm (manifoldStructure d x) (EuclideanSpace.single μ 1)
   simp [manifoldStructure]
   ext i
   rw [fderiv_space_components _ _ (by fun_prop)]
