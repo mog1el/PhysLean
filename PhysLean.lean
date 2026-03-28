57cb9b23f578841f9d9d9723b873439b5dffde98
Zhi Kai Pong <46996788+zhikaip@users.noreply.github.com>
2026-03-28 10:39:33 +0000

    feat: EM in three dimensions (basic relations) (#1010)
    * feat: EM in three dimensions (basic relations)
    
    * rename and add to Physlean.lean

--- PhysLean.lean
+++ PhysLean.lean
@@ -41,6 +41,7 @@ public import PhysLean.Electromagnetism.Kinematics.ScalarPotential
 public import PhysLean.Electromagnetism.Kinematics.VectorPotential
 public import PhysLean.Electromagnetism.PointParticle.OneDimension
 public import PhysLean.Electromagnetism.PointParticle.ThreeDimension
+public import PhysLean.Electromagnetism.ThreeDimension.Basic
 public import PhysLean.Electromagnetism.Vacuum.Constant
 public import PhysLean.Electromagnetism.Vacuum.HarmonicWave
 public import PhysLean.Electromagnetism.Vacuum.IsPlaneWave
